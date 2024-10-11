376c7c9c7c7f6a3886a37e1aa75b35f92e0d15e1
Bartosz Kaczor <bart.kaczor@gmail.com>
2024-10-11 11:51:20 +0200

    Add models

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+file = File.open("./vendor/recipes-en.json")
+recipes = JSON.parse(file.read)
+recipes.each do |recipe|
+  attrs = recipe.except("ingredients", "image")
+  attrs["image_url"] = recipe["image"]
+  new_recipe = Recipe.create!(attrs)
+
+  ingredients = recipe["ingredients"].map { |i| { name: i, recipe_id: new_recipe.id } }
+  Ingredient.insert_all(ingredients)
+end
