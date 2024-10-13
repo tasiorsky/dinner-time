30abe21f2f4b8b1733e32180b2035ce26b54d7a8
Bartosz Kaczor <bart.kaczor@gmail.com>
2024-10-13 12:22:29 +0200

    Add bootstrap

--- app/javascript/components/Recipes.jsx
+++ app/javascript/components/Recipes.jsx
@@ -4,14 +4,16 @@ import Recipe from "./Recipe";
 export default () => {
   const [recipes, setRecipes] = useState([]);
 
-  useEffect(() => {
+  useEffect(fetchRecipes, []);
+
+  function fetchRecipes() {
     fetch("/recipes")
       .then((res) => {
         if (res.ok) return res.json();
         throw new Error("Network response was not ok.");
       })
       .then((res) => setRecipes(res))
-  }, []);
+  }
 
   return <>{recipes.map((recipe) => (
     <Recipe
